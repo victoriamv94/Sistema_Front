ff5a6ca582b6b7629b1d7afca85ffe867d64efee
vmartinez <victoria.martinez@blackboard.com>
2022-05-13 15:01:32 -0500

    agregando diseño para el componente crear-clase

--- src/app/componentes/clase/crear-clase/crear-clase.component.ts
+++ src/app/componentes/clase/crear-clase/crear-clase.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MaestroService } from 'src/app/services/maestro.service';
 
 @Component({
   selector: 'app-crear-clase',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearClaseComponent implements OnInit {
 
-  constructor() { }
+  
+  form: FormGroup = new FormGroup({});
+
+  constructor(
+    public maestroService: MaestroService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+
+    this.form = new FormGroup({
+      nombre:  new FormControl('', [ Validators.required, Validators.pattern('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ \-\']+') ]),
+      salon: new FormControl('', [ Validators.required, Validators.pattern('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ \-\']+') ]),
+      maestro: new FormControl('', [ Validators.required, Validators.pattern('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ \-\']+') ]),
+    });
+
+  }
+
+  get f(){
+    return this.form.controls;
+  }
+
+  submit(){
+    console.log(this.form.value);
+    this.maestroService.create(this.form.value).subscribe(res => {
+         console.log('Maestro created successfully!');
+         this.router.navigateByUrl('listaMaestros');
+    })
   }
 
 }
